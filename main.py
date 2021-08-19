d78b5d944f0418357c4eff8ba8625c103cb67ae5
Gabriel Toro <torogabriel1@gmail.com>
2021-08-19 15:26:12 -0500

    Characters

--- main.py
+++ main.py
@@ -23,31 +23,109 @@ Blue_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 Yellow_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 # Background
-Background = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (Width, Height))
+Background = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")).convert(), (Width, Height))
+
+class Ship:
+    def __init__(self, x, y, health = 100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+
+    def get_width(self):
+        return self.ship_img.get_width()
+
+    def get_height(self):
+        return self.ship_img.get_height()
+
+class Player(Ship):
+    def __init__(self, x, y, health = 100):
+        super().__init__(x, y, health)
+        self.ship_img = Yellow_Space_Ship
+        self.laser_img = Yellow_Laser
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
+
+class Enemy(Ship):
+    Color_Map = {
+                "red": (Red_Space_Ship, Red_Laser),
+                "green": (Green_Space_Ship, Green_Laser),
+                "blue": (Blue_Space_Ship, Blue_Laser)
+                }
+
+    def __init__(self, x, y, color, health = 100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.Color_Map[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, vel):
+        self.y += vel
 
 def main():
     run = True
     FPS = 60
-    level = 1
+    level = 0
     lives = 5
     main_font = pygame.font.SysFont("TimesNewRoman", 50)
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
+
+    player_vel = 5
+
+    player = Player(300,650)
+
     clock = pygame.time.Clock()
 
     def redraw_window():
-        Window.blit(Background (0, 0))
+        Window.blit(Background, (0, 0))
         # Draw text
         lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
         level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
         Window.blit(lives_label, (10, 10))
         Window.blit(level_label, (Width - level_label.get_width() - 10, 10))
+
+        for enemy in enemies:
+            enemy.draw(Window)
+
+        player.draw(Window)
+
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
-        redraw_window()
+
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, Width - 100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
+                enemies.append(enemy)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and player.x - player_vel > 0: #left
+            player.x -= player_vel
+        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < Width: #right
+            player.x += player_vel
+        if keys[pygame.K_w] and player.y - player_vel > 0: #up
+            player.y -= player_vel
+        if keys[pygame.K_s] and player.y + player_vel + player.get_height() < Height: #down
+            player.y += player_vel
+
+        for enemy in enemies:
+            enemy.move(enemy_vel)
+
+        redraw_window()
 
 main()
